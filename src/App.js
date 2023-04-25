6fcb88d92251d3a1a65e82e243256c4bbc16c258
chucamo <khoaivu2k3@gmail.com>
2023-04-25 23:59:05 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./page/Home";
 import Slider from "./page/Slider";
 import Container from './page/Container';
 import Slider1 from "./page/Slider1";
+import Slider2 from "./page/Slider2";
 function App() {
   return (
     <div className="App">
@@ -12,7 +13,9 @@ function App() {
       <Slider/>
       <Container/>
       <Slider1/>
+
         <Home/>
+        <Slider2/>
         <Footer/>
     </div>
   );
