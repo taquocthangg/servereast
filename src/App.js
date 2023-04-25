8310dcf0e4fe05a6ac9c6afa989f729c3460d214
Ứng Trí <lamcongtri200@gmail.com>
2023-04-25 22:03:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Header } from "./component/Header/Header";
 import Footer from "./component/Footer/Footer";
 import Home from "./page/Home";
 import Slider from "./page/Slider";
+import Container from './page/Container';
 function App() {
   return (
     <div className="App">
       <Header/>
       <Slider/>
+      <Container/>
         <Home/>
         <Footer/>
     </div>
---
+++ src/page/Container.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../css/container.css'
+const Container = () => {
+  return (
+    <div>
+        <div className="container1">
+            <div>
+            <div className="container1_wrap">
+            <div className="container1_wrap1">GET <span>30%</span>OFF</div>
+            <div className="container1_wrap2">
+                <h2>GET STARTING TODAY</h2>
+                <p>by register a domain name</p>
+            </div>
+            </div>
+            <form action="" className="container1__form">
+                <input type="text" className="container1__input" placeholder="Enter your domain..."/>
+                <button className="btn container1__btn">SEARCH</button>
+            </form>
+            </div>
+         </div>
+         <div className="container2">
+            
+         </div>
+    </div>
+  )
+}
+
+export default Container
